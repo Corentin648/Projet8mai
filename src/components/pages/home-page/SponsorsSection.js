12ea090ab555421da8e89c810f6352348c14a036
Corentin648 <corentin.dmz@live.fr>
2022-01-21 20:27:33 +0200

    section sponsors responsive

--- src/components/pages/home-page/SponsorsSection.js
+++ src/components/pages/home-page/SponsorsSection.js
@@ -21,14 +21,6 @@ export default class SponsorsSection extends Component {
 
         function estimateSponsorsContainerClass() {
             const elements = document.querySelectorAll('.sponsors-container');
-            /*
-                        function isInViewport(element) {
-                            const rect = element.getBoundingClientRect();
-                            return ((
-                                ((rect.top >= 0 && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) || rect.top <= 0) &&
-                                ((rect.left >= 0 && rect.left <= (window.innerWidth || document.documentElement.clientWidth)) || rect.left <= 0)
-                            ));
-                        }*/
 
             elements.forEach((el) => {
                 if (utils.isInViewport(el)
